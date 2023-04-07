087c60d78fcb5aa8a46ea43d21635434531cb06d
李卡卡a <10328376+likakaas@user.noreply.gitee.com>
2023-04-07 14:01:49 +0800

    test ssg

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import type { ConfigEnv } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }: ConfigEnv) => {
   return {
-    base: "/vue3-vite",
+    base: "/",
     plugins: [
       vue(),
       // https://vite-pwa-org.netlify.app/guide/
